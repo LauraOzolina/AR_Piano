eabe415b509f6ccddd2b53013eace0e838cfa81a
LauraOzolina <laura.ozolina15@va.lv>
2021-03-08 23:16:39 +0200

    check played notes

--- Assets/buttonsound.cs
+++ Assets/buttonsound.cs
@@ -12,8 +12,16 @@ public class buttonsound : MonoBehaviour
     public AudioClip clipTarget;
     private AudioSource[] allAudioSources;
     public TextMesh notes;
+    char[] od, tw;
     TextMesh mode_text, song_text, notes_text;
     VirtualButtonBehaviour[] virtualButtonBehaviours;
+    string ode_to_joy;
+    string twinkle;
+    string played_music;
+    int which_song;
+    bool record;
+    int total;
+    int correct;
     // Use this for initialization
     //function to stop all sounds
     void StopAllAudio()
@@ -33,7 +41,7 @@ public class buttonsound : MonoBehaviour
         soundTarget.loop = false;
         soundTarget.playOnAwake = false;
         soundTarget.PlayOneShot(soundTarget.clip);
-        Debug.Log(ss);
+   
     }
     void Start()
     {
@@ -41,73 +49,111 @@ public class buttonsound : MonoBehaviour
         virtualButtonBehaviours = GetComponentsInChildren<VirtualButtonBehaviour>();
         mode_text = mode_btn.GetComponentInChildren<TextMesh>();
         song_text = song_btn.GetComponentInChildren<TextMesh>();
-
+        ode_to_joy = "EEFGGFEDCCDEEDDEEFGGFEDCCDEDCCDDECEFECDEFEDCDGEEFGGFEDCCDEDCC";
+        twinkle = "CCGGAAGFFEEDDCGGFFEEDGGFFEEDCCGGAAGFFEEDDC";
         notes_text = notes.GetComponentInChildren<TextMesh>();
-
+        which_song = 0;
+        total = 0;
+        correct = 0;
+        record = false;
+        played_music = "";
+        // od= ode_to_joy.ToCharArray();
+        //array_of_notes[2] = twinkle.ToCharArray();
         for (int i = 0; i < virtualButtonBehaviours.Length; i++)
         {
             virtualButtonBehaviours[i].GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
             virtualButtonBehaviours[i].GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
         }
         vbBtnObj = GameObject.Find("c_button");
-        // audioSource = GetComponent<AudioSource>();
-        Debug.Log(vbBtnObj);
-        Debug.Log("skanas skripts");
-        //  vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonPressed(OnButtonPressed);
-        // vbBtnObj.GetComponent<VirtualButtonBehaviour>().RegisterOnButtonReleased(OnButtonReleased);
+
+     
+
     }
     void Update()
     {
-        if ((mode_text.text == "Start") && (song_text.text == "Ode To Joy"))
-        {
-            notes_text.text = "E E F G G F E D C \n C D E E D D \n E E F G G F E D C C D\n E D C C D D E C  \nD E F E C D E F E D C D G E E F \n G G F E D C C D E D C C";
-        }
-        else if ((mode_text.text == "Start") && (song_text.text == "Twinkle Twinkle Little Star"))
-        {
-            notes_text.text = "C C G G A A G\n F F E E D D C\n G G F F E E D\n G G F F E E D\n C C G G A A G F F E E D D C";
-        }
-        else
-        {
-            notes_text.text = "notes";
-        }
+
 
 
     }
     public void OnButtonPressed(VirtualButtonBehaviour vb)
     {
-
-
+  Debug.Log(record);
+        if ((mode_text.text == "Stop") && (song_text.text == "Ode To Joy"))
+        {
+            which_song = 1;
+            record = true;
+          
+        }
+  
+        if ((mode_text.text == "Stop") && (song_text.text == "Twinkle Twinkle Little Star"))
+        {
+            which_song = 2;
+            record = true;
+            Debug.Log(vb.name + "notietwgu");
+        }
+ 
         Debug.Log(vb.name + "Button pressed");
         switch (vb.name)
         {
             case "c_button":
                 playSound("sounds/cnote");
+                if (record)
+                {
+                    played_music += "C";
+                }
                 break;
 
             case "d_button":
                 playSound("sounds/dnote");
-                print("dnots");
+                if (record)
+                {
+                    played_music += "D";
+                }
                 break;
+
             case "e_button":
                 playSound("sounds/enote");
-                print("enots");
+                if (record)
+                {
+                    played_music += "E";
+                    Debug.Log(" / gno");
+                }
+              
                 break;
+
             case "f_button":
                 playSound("sounds/fnote");
-                print("fnots");
+                if (record)
+                {
+                    played_music += "F"; 
+                }
                 break;
+
             case "g_button":
                 playSound("sounds/gnote");
-                print("gnots");
+                if (record)
+                {
+                    played_music += "G"; 
+
+                }
                 break;
+
             case "a_button":
                 playSound("sounds/anote");
-                print("anots");
+                if (record)
+                {
+                    played_music += "A";
+                }
                 break;
+
             case "b_button":
                 playSound("sounds/bnote");
-                print("bnots");
+                if (record)
+                {
+                    played_music += "B";
+                }
                 break;
+
             case "mode_button":
                 if (mode_text.text == "Practice")
                 {
@@ -120,33 +166,130 @@ public class buttonsound : MonoBehaviour
                 else
                 {
                     mode_text.text = "Practice";
+                    switch (which_song)
+                    {
+                        case 0:
+                            break;
+
+                        case 1:
+                            total = 0;
+                            correct = 0;
+                            Debug.Log(played_music); 
+                            foreach (char c in ode_to_joy)
+                            {
+                                Debug.Log(total);
+                                if (total >= 0 && total < played_music.Length - 1)
+                                {
+                                    if (played_music[total] == c)
+                                    {
+                                        correct++;
+
+
+                                    }
+                                    else
+                                    {
+
+                                    }
+
+                                }
+                                total++;
+                            }
+                            notes_text.text = "Result:" + correct + "/" + total;
+                            played_music = "";
+                            break;
+
+                        case 2:
+                            Debug.Log(played_music);
+                            total = 0;
+                            correct = 0;
+                            foreach (char c in twinkle)
+                            {
+                                Debug.Log(total); 
+                                Debug.Log(played_music);
+                                if (total >= 0 && total < played_music.Length - 1)
+                                {
+                                    if (played_music[total] == c)
+                                    {
+                                        correct++;
+
+
+                                    }
+                                    else
+                                    {
+
+                                    }
+
+                                }
+                                total++;
+
+                            }
+                            notes_text.text = "Result:" + correct + "/" + total;
+                            played_music = "";
+                            break;
+                    }
                 }
-                print(mode_text.text);
+            
                 break;
+
             case "song_button":
                 if (song_text.text == "Choose song")
                 {
                     song_text.text = "Ode To Joy";
+                    which_song = 1;
+                    int j = 0;
+                    string to_display = "";
+                    foreach (char c in ode_to_joy)
+                    {
+                        ++j;
+                        to_display += c.ToString();
+                        if (j > 8)
+                        {
+                            j = 0;
+                            to_display += "\n";
+                        }
+                    }
+                    notes_text.text = to_display;
+
                 }
                 else if (song_text.text == "Ode To Joy")
                 {
                     song_text.text = "Twinkle Twinkle Little Star";
+                    which_song = 2;
+                    int j = 0;
+                    string to_display = "";
+                    foreach (char c in twinkle)
+                    {
+                        ++j;
+                        to_display += c.ToString();
+                        if (j > 8)
+                        {
+                            j = 0;
+                            to_display += "\n";
+                        }
+                    }
+                    notes_text.text = to_display;
+
                 }
                 else
                 {
                     song_text.text = "Choose song";
+
                 }
-                print(song_text.text);
+               
                 break;
+
             default:
                 print("nav nekâ");
                 break;
         }
+        print("______________________");
+        print(record);
+        print(played_music);
+
     }
 
     public void OnButtonReleased(VirtualButtonBehaviour vb)
     {
-        // StopAllAudio();
-        Debug.Log("Button released");
+     
     }
 }
